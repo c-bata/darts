3fce0cbcee886c2e1e8ad727582b27cc834f636d
c-bata <contact@c-bata.link>
2019-11-21 15:10:22 +0900

    Avoid to use data attribute

--- cnn/architect.py
+++ cnn/architect.py
@@ -85,7 +85,7 @@ class Architect:
         implicit_grads = self._hessian_vector_product(vector, input_train, target_train)
 
         for g, ig in zip(dalpha, implicit_grads):
-            g.data.sub_(eta, ig.data)
+            g.sub_(eta, ig)
 
         for v, g in zip(self.model.arch_parameters(), dalpha):
             if v.grad is None:
@@ -111,16 +111,16 @@ class Architect:
     def _hessian_vector_product(self, vector, input, target, r=1e-2):
         R = r / _concat(vector).norm()
         for p, v in zip(self.model.parameters(), vector):
-            p.data.add_(R, v)
+            p.add_(R, v)
         loss = self.model._loss(input, target)
         grads_p = torch.autograd.grad(loss, self.model.arch_parameters())
 
         for p, v in zip(self.model.parameters(), vector):
-            p.data.sub_(2 * R, v)
+            p.sub_(2 * R, v)
         loss = self.model._loss(input, target)
         grads_n = torch.autograd.grad(loss, self.model.arch_parameters())
 
         for p, v in zip(self.model.parameters(), vector):
-            p.data.add_(R, v)
+            p.add_(R, v)
 
         return [(x - y).div_(2 * R) for x, y in zip(grads_p, grads_n)]
