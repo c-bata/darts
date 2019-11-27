dda7a573f7c32eaeb75b17140ea8e49b79bd0424
c-bata <contact@c-bata.link>
2019-11-27 14:17:32 +0900

    Add comments for code reading

--- cnn/model_search.py
+++ cnn/model_search.py
@@ -8,24 +8,70 @@ from genotypes import Genotype
 
 
 class MixedOp(nn.Module):
-    def __init__(self, C, stride):
-        super(MixedOp, self).__init__()
+    _ops: nn.ModuleList
+
+    def __init__(self, C: int, stride: int):
+        super().__init__()
         self._ops = nn.ModuleList()
+
         for primitive in PRIMITIVES:
-            op = OPS[primitive](C, stride, False)
-            if "pool" in primitive:
+            op: nn.Module = OPS[primitive](C, stride, False)
+
+            if 'pool' in primitive:  # avg_pool_3x3 or max_pool_3x3
+                # poolingがあったらとりあえず思考停止でBatchNorm2dかませる。
                 op = nn.Sequential(op, nn.BatchNorm2d(C, affine=False))
             self._ops.append(op)
 
     def forward(self, x, weights):
-        return sum(w * op(x) for w, op in zip(weights, self._ops))
+        # 雰囲気的には、各operationの選択確率がweightsとして渡されるから、
+        # それをcontinuous domain(連続値の空間)に落とし込んでいるようにみえる。
+        # TODO(c-bata): nn.Module と weight の積が何を意味するか確認
+        s = sum(w * op(x) for w, op in zip(weights, self._ops))
+        # p weights[0]: [torch.cuda.FloatTensor of size 1 (GPU 0)]
+        """ weights は最初こんな感じ
+        (Pdb) p weights
+        Variable containing:
+         0.1249
+         0.1252
+         0.1250
+         0.1249
+         0.1250
+         0.1250
+         0.1250
+         0.1251
+        [torch.cuda.FloatTensor of size 8 (GPU 0)]
+        (pdb) p sum(weights)
+        Variable containing:
+         1
+        [torch.cuda.FloatTensor of size 1 (GPU 0)]
+        """
+
+        # x: torch.cuda.FloatTensor of size 64x16x32x32 (GPU 0)]
+        # self._ops[0](x): [torch.cuda.FloatTensor of size 64x16x32x32 (GPU 0)]
+        # s: [torch.cuda.FloatTensor of size 64x16x32x32 (GPU 0)]
+        # weights は スカラーだから ops[0](x) の次元を変えない
+        return s
 
 
 class Cell(nn.Module):
+    preprocess0: nn.Module
+    preprocess1: nn.Module
+    _steps: int
+    _multiplier: int
+    _ops: nn.ModuleList  # MixedOp がappendされていく。
+    _bns: nn.ModuleList
+
     def __init__(
-        self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev
+            self,
+            steps: int,
+            multiplier: int,  # 今のところ用途が謎。Networkクラスで使ってるっぽい。
+            C_prev_prev: int,  # 2つ前のcellの出力の次元っぽい
+            C_prev: int,  # 1つ前のcellの出力の次元っぽい
+            C: int,  # 自身のCellの入力 or 出力の次元。どっちかはFactorizedReduceが何やってるか調べたらいいかも。
+            reduction: bool,  # strideを2にして次元を落とすかを指定
+            reduction_prev: bool,  # 1つ前のcellがstrideを2にして次元を落としたかどうかを保持
     ):
-        super(Cell, self).__init__()
+        super().__init__()
         self.reduction = reduction
 
         if reduction_prev:
@@ -67,10 +113,10 @@ class Network(nn.Module):
         C,
         num_classes,
         layers,
-        criterion,
+        criterion,  # criterion=基準. nn.CrossEntropyLoss() とかが渡されてくる.
         steps=4,
         multiplier=4,
-        stem_multiplier=3,
+        stem_multiplier=3,  # Conv2d層で何倍に次元を増やすかを指定。多いほどおそらく表現力が上がるけど、学習が難しくなるんだと思う
     ):
         super(Network, self).__init__()
         self._C = C
@@ -80,6 +126,7 @@ class Network(nn.Module):
         self._steps = steps
         self._multiplier = multiplier
 
+        # 入力 3次元、出力 C_curr 次元の畳み込み層は必ず最初に通すようにしている。つまりこの層はアーキテクチャの探索を行わない。
         C_curr = stem_multiplier * C
         self.stem = nn.Sequential(
             nn.Conv2d(3, C_curr, 3, padding=1, bias=False), nn.BatchNorm2d(C_curr)
@@ -90,6 +137,36 @@ class Network(nn.Module):
         reduction_prev = False
         for i in range(layers):
             if i in [layers // 3, 2 * layers // 3]:
+                # if i in [2, 5]: 必ず2つreductionレイヤーを入れるようにしてる。
+                """
+                >>> def print_reduction(layers):
+                ...     for i in range(layers):
+                ...         hits = [layers // 3, 2 * layers // 3]
+                ...         print(f"layer_{i}: {i in hits} : {hits}")
+                ...
+                >>> print_reduction(8)
+                layer_0: False : [2, 5]
+                layer_1: False : [2, 5]
+                layer_2: True : [2, 5]
+                layer_3: False : [2, 5]
+                layer_4: False : [2, 5]
+                layer_5: True : [2, 5]
+                layer_6: False : [2, 5]
+                layer_7: False : [2, 5]
+                >>> print_reduction(12)
+                layer_0: False : [4, 8]
+                layer_1: False : [4, 8]
+                layer_2: False : [4, 8]
+                layer_3: False : [4, 8]
+                layer_4: True : [4, 8]
+                layer_5: False : [4, 8]
+                layer_6: False : [4, 8]
+                layer_7: False : [4, 8]
+                layer_8: True : [4, 8]
+                layer_9: False : [4, 8]
+                layer_10: False : [4, 8]
+                layer_11: False : [4, 8]
+                """
                 C_curr *= 2
                 reduction = True
             else:
@@ -105,6 +182,9 @@ class Network(nn.Module):
             )
             reduction_prev = reduction
             self.cells += [cell]
+            # なぜか C_prev は C_curr に multiplier をかけている。
+            # 1つ前のCellの次元は、今回の入力次元にmultiplier(4)をかけたものらしい。
+
             C_prev_prev, C_prev = C_prev, multiplier * C_curr
 
         self.global_pooling = nn.AdaptiveAvgPool2d(1)
@@ -121,15 +201,31 @@ class Network(nn.Module):
         return model_new
 
     def forward(self, input):
+        # CIFAR 10 の画像データセットは、 32x32 の画像が60000枚入っていて、それぞれ10クラスに分類されている。
+        # RGBの3 channelがあるっぽい。
+        # input: [torch.cuda.FloatTensor of size 64x3x32x32 (GPU 0)]
+
         s0 = s1 = self.stem(input)
+        # s0: [torch.cuda.FloatTensor of size 64x48x32x32 (GPU 0)]
+
         for i, cell in enumerate(self.cells):
             if cell.reduction:
                 weights = F.softmax(self.alphas_reduce, dim=-1)
             else:
+                # self.alphas_normal: [torch.cuda.FloatTensor of size 14x8 (GPU 0)]
                 weights = F.softmax(self.alphas_normal, dim=-1)
+            # weights: [torch.cuda.FloatTensor of size 14x8 (GPU 0)]
+
             s0, s1 = s1, cell(s0, s1, weights)
+            # s0: [torch.cuda.FloatTensor of size 64x48x32x32 (GPU 0)]
+            # s1: [torch.cuda.FloatTensor of size 64x64x32x32 (GPU 0)]
         out = self.global_pooling(s1)
+        # out: [torch.cuda.FloatTensor of size 64x256x1x1 (GPU 0)]
+        # out.view(out.size(0), -1): [torch.cuda.FloatTensor of size 64x256 (GPU 0)]
+
         logits = self.classifier(out.view(out.size(0), -1))
+        # logits: [torch.cuda.FloatTensor of size 64x10 (GPU 0)]
+
         return logits
 
     def _loss(self, input, target):
--- cnn/operations.py
+++ cnn/operations.py
@@ -25,12 +25,6 @@ OPS = {
     "dil_conv_5x5": lambda C, stride, affine: DilConv(
         C, C, 5, stride, 4, 2, affine=affine
     ),
-    "conv_7x1_1x7": lambda C, stride, affine: nn.Sequential(
-        nn.ReLU(inplace=False),
-        nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
-        nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
-        nn.BatchNorm2d(C, affine=affine),
-    ),
 }
 
 
@@ -117,6 +111,7 @@ class Identity(nn.Module):
 
 
 class Zero(nn.Module):
+    """ノード同士を繋がないという操作"""
     def __init__(self, stride):
         super(Zero, self).__init__()
         self.stride = stride
